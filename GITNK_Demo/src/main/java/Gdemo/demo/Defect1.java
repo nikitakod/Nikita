06be3d2b123ef678e3be757558887db4d749dac2
nikitakod <nikitakod@nikitakod-win8.cybage.com>
2019-06-13 19:07:31 +0530

    Adding defects

---
+++ GITNK_Demo/src/main/java/Gdemo/demo/Defect1.java
@@ -0,0 +1,12 @@
+package Gdemo.demo;
+
+public class Defect1 {
+	
+	
+
+		public static void main(String args[]){
+			// TODO Auto-generated method stub
+			System.out.println("Hello,Java");
+		}
+	}
+
---
+++ GITNK_Demo/src/main/java/Gdemo/demo/Defect2.java
@@ -0,0 +1,11 @@
+package Gdemo.demo;
+
+public class Defect2 {
+	
+	
+
+		public static void main(String args[]){
+			// TODO Auto-generated method stub
+			System.out.println("Hello,Java");
+		}
+	}
